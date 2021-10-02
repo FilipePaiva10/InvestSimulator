b114c6aaaed01a6784f7d8b5a5897c50c05e5cb6
hidoshino <91507697+FilipePaiva10@users.noreply.github.com>
2021-10-02 20:52:21 -0300

    Criação do projeto

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { SafeAreaView, Text } from 'react-native'
+
+export default () => {
+
+  return (
+    <SafeAreaView>
+      <Text>InvestSimulator</Text>
+    </SafeAreaView>
+  )
+
+}
